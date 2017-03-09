b35b1285d5e25c66cc781f5ecfdef33573150d94
dreamer2018 <zhoupans_mail@163.com>
2017-03-09 09:02:59 +0800

    加入字符串转换规则,防止漏网之鱼

--- src/org/xiyoulinux/util/HtmlUtil.java
+++ src/org/xiyoulinux/util/HtmlUtil.java
@@ -3,11 +3,9 @@ package org.xiyoulinux.util;
 /**
  * Created by zhoupan on 11/30/16.
  */
+
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
-
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 public class HtmlUtil {
     public static String getTextFromTHML(String htmlStr) {
@@ -16,14 +14,23 @@ public class HtmlUtil {
         // remove extra white space
         StringBuilder builder = new StringBuilder(text);
         int index = 0;
-        while(builder.length()>index){
+        while (builder.length() > index) {
             char tmp = builder.charAt(index);
-            if(Character.isSpaceChar(tmp) || Character.isWhitespace(tmp)){
+            if (Character.isSpaceChar(tmp) || Character.isWhitespace(tmp)) {
                 builder.setCharAt(index, ' ');
             }
             index++;
         }
+        //去除< > ' " 防止漏网之鱼
         text = builder.toString().replaceAll(" +", " ").trim();
-        return text;
+        String text1 = text.replace("<", "&lt;");
+        String text2 = text1.replace(">", "&gt;");
+        String text3 = text2.replace("'", "&amp;");
+        return  text3.replace("\"", "&quot;");
+    }
+
+    public static void main(String[] args) {
+        String text = "\"/><script>alert(\"haha\");</script><!--ssssssssssssssss";
+        System.out.println(HtmlUtil.getTextFromTHML("fdsfdsfsvf>><script></script>&lt;&gt;"));
     }
 }
