af38f8dbfdb2ccb16cf5072ab92a3a1f18c2a6ae
dreamer2018 <zhoupans_mail@163.com>
2017-03-28 15:16:11 +0800

    修复json获取bug

--- src/DomTest.java
+++ src/DomTest.java
@@ -7,10 +7,12 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.xiyoulinux.dao.BlogDAO;
 import org.xiyoulinux.model.Blog;
+import sun.swing.BakedArrayList;
 
 import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 
 
@@ -18,37 +20,61 @@ import java.util.Date;
  * Created by zhoupan on 11/6/16.
  */
 public class DomTest {
-    public static void main(String[] args) throws IOException, ParseException {
-        Document doc = Jsoup.connect("http://blog.xiyoulinux.org").get();
+    public static void main(String[] args) {
+        ArrayList<Blog> blogs = DomTest.getBlogFromHtml();
+        if (null != blogs) {
+            for (Blog blog : blogs) {
+                System.out.println("地址：" + blog.getUrl());
+                System.out.println("标题：" + blog.getTitle());
+                System.out.println("作者：" + blog.getAuthor());
+                System.out.println("时间：" + blog.getDate() + " " + blog.getTime());
+                System.out.println("内容：" + blog.getSummary());
+                System.out.println("__________________________________________");
+            }
+        }
+    }
+
+    private static ArrayList<Blog> getBlogFromHtml() {
+        Document doc;
+        try {
+            doc = Jsoup.connect("http://blog.xiyoulinux.org").get();
+        } catch (IOException ie) {
+            System.out.println("\nGet Blog From http://blog.xiyoulinux.org");
+            System.out.println("JSON Resolve Error :  " + ie.toString());
+            System.out.println("================================================");
+            return null;
+        }
         Element bbox = doc.getElementById("bbox");
         Elements vbox = bbox.getElementsByClass("vbox");
+        ArrayList<Blog> blogs = new ArrayList<>();
         for (int i = 0; i < vbox.size(); i++) {
+            Blog blog = new Blog();
             Elements node = vbox.get(i).getElementsByClass("node");
             Elements link = vbox.get(i).getElementsByClass("more-link");
             Elements fsize = vbox.get(i).getElementsByClass("fsize");
             Elements title = fsize.get(0).getElementsByTag("a");
             Elements afont = vbox.get(i).getElementsByClass("afont");
             Elements time = vbox.get(i).getElementsByClass("time");
-            System.out.println("地址：" + link.get(0).attr("href"));
-            System.out.println("标题：" + title.get(0).text());
-            System.out.println("作者：" + afont.get(0).text());
-            System.out.println("时间：" + time.get(0).text());
-            System.out.println("内容：" + node.get(0).text());
-            System.out.println("_______________________________________________");
-
             SimpleDateFormat sdf = new SimpleDateFormat("yy/MM/dd HH:mm:ss");
-            Date date = sdf.parse(time.get(0).text());
+            Date date;
+            try {
+                date = sdf.parse(time.get(0).text());
+            } catch (ParseException pe) {
+                System.out.println("\nGet Blog From http://blog.xiyoulinux.org");
+                System.out.println("JSON Resolve Error :  " + pe.toString());
+                System.out.println("================================================");
+                return null;
+            }
             SimpleDateFormat d = new SimpleDateFormat("yyyy-MM-dd");
             SimpleDateFormat t = new SimpleDateFormat("HH:mm:ss");
-            BlogDAO blogDAO = new BlogDAO();
-            Blog blog = new Blog();
             blog.setTitle(title.get(0).text());
             blog.setAuthor(afont.get(0).text());
+            blog.setUrl("http://blog.xiyoulinux.org/" + link.get(0).attr("href"));
             blog.setDate(d.format(date));
             blog.setTime(t.format(date));
             blog.setSummary(node.get(0).text());
-            blog.setUrl(link.get(0).attr("href"));
-            blogDAO.insert(blog);
+            blogs.add(blog);
         }
+        return blogs;
     }
 }
--- src/org/xiyoulinux/util/Blogjson.java
+++ src/org/xiyoulinux/util/Blogjson.java
@@ -3,6 +3,7 @@ package org.xiyoulinux.util;
 import org.json.JSONObject;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 import org.xiyoulinux.dao.BlogDAO;
 import org.xiyoulinux.model.Blog;
@@ -18,7 +19,54 @@ import java.util.Date;
  * Created by zhoupan on 11/30/16.
  */
 public class Blogjson {
-    public static ArrayList<Blog> getBlogFromJson() {
+    private static ArrayList<Blog> getBlogFromHtml() {
+        Document doc;
+        try {
+            doc = Jsoup.connect("http://blog.xiyoulinux.org").get();
+        } catch (IOException ie) {
+            System.out.println("\nGet Blog From http://blog.xiyoulinux.org");
+            System.out.println("JSON Resolve Error :  " + ie.toString());
+            System.out.println("================================================");
+            return null;
+        }
+        Element bbox = doc.getElementById("bbox");
+        Elements vbox = bbox.getElementsByClass("vbox");
+        ArrayList<Blog> blogs = new ArrayList<>();
+        for (int i = 0; i < vbox.size(); i++) {
+            Blog blog = new Blog();
+            Elements node = vbox.get(i).getElementsByClass("node");
+            Elements link = vbox.get(i).getElementsByClass("more-link");
+            Elements fsize = vbox.get(i).getElementsByClass("fsize");
+            Elements title = fsize.get(0).getElementsByTag("a");
+            Elements afont = vbox.get(i).getElementsByClass("afont");
+            Elements time = vbox.get(i).getElementsByClass("time");
+            SimpleDateFormat sdf = new SimpleDateFormat("yy/MM/dd HH:mm:ss");
+            Date date;
+            try {
+                date = sdf.parse(time.get(0).text());
+            } catch (ParseException pe) {
+                System.out.println("\nGet Blog From http://blog.xiyoulinux.org");
+                System.out.println("JSON Resolve Error :  " + pe.toString());
+                System.out.println("================================================");
+                return null;
+            }
+            SimpleDateFormat d = new SimpleDateFormat("yyyy-MM-dd");
+            SimpleDateFormat t = new SimpleDateFormat("HH:mm:ss");
+            blog.setTitle(title.get(0).text());
+            blog.setAuthor(afont.get(0).text());
+            blog.setUrl("http://blog.xiyoulinux.org/" + link.get(0).attr("href"));
+            blog.setDate(d.format(date));
+            blog.setTime(t.format(date));
+            blog.setSummary(node.get(0).text());
+            blogs.add(blog);
+        }
+        System.out.println("\nGet Blog From http://blog.xiyoulinux.org");
+        System.out.println("Blog Count:\t" + blogs.size());
+        System.out.println("================================================");
+        return blogs;
+    }
+
+    private static ArrayList<Blog> getBlogFromJson() {
         Document doc;
         //如果地址访问错误，则跳过，不影响定时器继续执行
         try {
@@ -26,8 +74,7 @@ public class Blogjson {
         } catch (IOException ie) {
             System.out.println("\nGet Blog From http://blog.xiyoulinux.org/blogjson");
             System.out.println("JSON Resolve Error :  " + ie.toString());
-            System.out.println("================================================");
-            return null;
+            return getBlogFromHtml();
         }
         Elements js = doc.getElementsByTag("body");
         String jsonString = js.html();
@@ -39,15 +86,14 @@ public class Blogjson {
         } catch (JSONException je) {
             System.out.println("\nGet Blog From http://blog.xiyoulinux.org/blogjson");
             System.out.println("JSON Resolve Error :  " + je.toString());
-            System.out.println("================================================");
-            return null;
+            return getBlogFromHtml();
         }
         for (int i = 0; i < 10; i++) {
             String key = "blog-" + i;
             JSONObject jsonObject = jsonObjects.getJSONObject(key);
             String datetime = jsonObject.getString("PubDate");
             SimpleDateFormat sdf = new SimpleDateFormat("yy-MM-dd HH:mm:ss");
-            Date date = null;
+            Date date;
             SimpleDateFormat d = new SimpleDateFormat("yyyy-MM-dd");
             SimpleDateFormat t = new SimpleDateFormat("HH:mm:ss");
             try {
@@ -81,6 +127,9 @@ public class Blogjson {
             //写入数据库
             blogs.add(blog);
         }
+        System.out.println("\nGet Blog From http://blog.xiyoulinux.org/blogjson");
+        System.out.println("Blog Count:\t" + blogs.size());
+        System.out.println("================================================");
         return blogs;
     }
 
@@ -90,9 +139,6 @@ public class Blogjson {
         if (null == blogs) {
             return;
         }
-        System.out.println("\nGet Blog From http://blog.xiyoulinux.org/blogjson");
-        System.out.println("Blog Count:\t" + blogs.size());
-        System.out.println("================================================");
         for (Blog blog : blogs) {
             // 看文章里面有没有此文章，有的话，就不再存储
             ArrayList<Blog> b = blogDAO.getBlogByTitle(blog.getTitle());
